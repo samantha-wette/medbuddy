2f94d6de7ed1207cd89b463f9cc1510485433888
Samantha Wette <samantha.wette@gmail.com>
2022-05-02 12:31:25 -0700

    database created, class User created

--- model.py
+++ model.py
@@ -1,1 +1,49 @@
 #write sqlalchemy classes into, communicate w/ db in py (translates sql to py)
+
+"""Models for medication tracker app"""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    """A user."""
+
+    __tablename__ = 'users'
+
+    user_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)
+    email = db.Column(db.String,
+                        unique=True,
+                        nullable=False)
+    password = db.Column(db.String,
+                        nullable=False)
+    fname = db.Column(db.String,
+                        nullable=False)
+    lname = db.Column(db.String)
+    points = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'<User user_id={self.user_id} email={self.email}>'
+
+def connect_to_db(flask_app, db_uri="postgresql:///med_tracker", echo=True):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_ECHO"] = echo
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print("Connected to the medtracker db!")
+
+
+if __name__ == "__main__":
+    from server import app
+
+    # Call connect_to_db(app, echo=False) if your program output gets
+    # too annoying; this will tell SQLAlchemy not to print out every
+    # query it executes.
+
+    connect_to_db(app)
+
---
+++ server.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+# Replace this with routes and view functions!
+
+
+if __name__ == "__main__":
+    # DebugToolbarExtension(app)
+
+    app.run(host="0.0.0.0", debug=True)
